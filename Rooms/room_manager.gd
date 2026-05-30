d5dfe2aa773adc3307c15ba3f7c3a2545f98e18b
Lluke18 <lucianlesustefan@gmail.com>
2026-05-30 18:35:51 +0300

    commit bc im scared

--- Rooms/room_manager.gd
+++ Rooms/room_manager.gd
@@ -7,10 +7,14 @@ extends Node
 
 
 func _ready() -> void:
+	print("ENTERED MANAGER")
 	room1.visible = true
 	room2.visible = false
+	room2.process_mode = Node.PROCESS_MODE_DISABLED
 	room3.visible = false
+	room3.process_mode = Node.PROCESS_MODE_DISABLED
 	room4.visible = false
+	room4.process_mode = Node.PROCESS_MODE_DISABLED
 	
 
 func switch_to_room2():
@@ -25,3 +29,7 @@ func _on_room_1_switch_room(no: int) -> void:
 		2:
 			print("switching to room2!")
 			switch_to_room2()
+			
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("3D_right"):
+		switch_to_room2()
