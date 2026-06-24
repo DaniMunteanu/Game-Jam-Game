706e4cac705ac957e012a464bcd5fabab051aa2c
Lluke18 <lucianlesustefan@gmail.com>
2026-06-24 13:26:29 +0300

    Squashed commit of the following:
    commit 4e52fd28a397de8fa86a9d660dcaf21dfa3db59a
    Merge: c3ba1b2 63e5dc3
    Author: Dani Munteanu <93090044+DaniMunteanu@users.noreply.github.com>
    Date:   Wed Jun 24 05:56:07 2026 +0300
    
        Merge pull request #149 from DaniMunteanu/main
    
        Added new items + gate stays open
        I tried to make the worm interactable, but it doesn't register the interaction yet. Will look into it more tommorow
    
    commit 63e5dc3ebfee5252df0269ffbbfd73d872325ee2
    Author: Dani Munteanu <daniel.munteanu@student.upt.ro>
    Date:   Wed Jun 24 05:54:14 2026 +0300
    
        Added the other items + added to aquarium
    
        I tried to make the worm interactable, but it doesn't register the interaction
    
    commit e3245d23758863717395879de156a0d1bd46361c
    Author: Dani Munteanu <daniel.munteanu@student.upt.ro>
    Date:   Wed Jun 24 00:18:35 2026 +0300
    
        Gate now stays open after opening it once

---
+++ Scripts/Puzzles2D/aquarium.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var fih: Sprite2D = $Fih
+@onready var gate_key: TextureButton = $GateKey
+@onready var item_drop_area: ItemDropArea = $ItemDropArea
+
+func _ready() -> void:
+	if PuzzleManager.worm_placed:
+		place_worm()
+	else:
+		item_drop_area.area_complete.connect(place_worm)
+		fih.visible = true
+		gate_key.disabled = true
+	
+func place_worm():
+	PuzzleManager.worm_placed = true
+	fih.visible = false
+	gate_key.disabled = false
+
+func _on_gate_key_pressed() -> void:
+	gate_key.queue_free()
+	InventoryManager.add_item(InventoryManager.GATE_KEY)
+	PuzzleManager.has_gate_key = true
