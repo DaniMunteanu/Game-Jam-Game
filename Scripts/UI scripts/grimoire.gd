e20367db4c3e0e9b0a3b56362642b15fbf1a8618
Dani Munteanu <daniel.munteanu@student.upt.ro>
2026-06-29 06:58:22 +0300

    Grimoire complete

--- Scripts/UI scripts/grimoire.gd
+++ Scripts/UI scripts/grimoire.gd
@@ -1,10 +1,49 @@
 extends Node
 
-@onready var pages: Control = $Pages
+@onready var pages_parent: Control = $Pages
+
+@onready var page_left_button: Button = $Pages/PageLeftButton
+@onready var page_right_button: Button = $Pages/PageRightButton
+@onready var close_button: Button = $Pages/CloseButton
+
+@export var pages: Array[GrimoirePage] = []
+
+@export var is_in_3d: bool = false
+
+var current_page_index: int = 0
+const MAX_PAGES: int = 5
 
 func _ready() -> void:
-	pages.hide()
+	pages_parent.hide()
+	current_page_index = 0
+	pages[0].visible = true
+	pages[0].update_cards()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("open_grimoire"):
-		pages.visible = !pages.visible
+		pages_parent.visible = !pages_parent.visible
+	
+	if pages_parent.visible:
+		pages[current_page_index].update_cards()
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		if is_in_3d:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		
+func show_page(page_index: int):
+	for page in pages:
+		page.visible = false
+	pages[page_index].visible = true
+	pages[page_index].update_cards()
+	current_page_index = page_index
+
+func _on_page_left_button_pressed() -> void:
+	if current_page_index > 0:
+		show_page(current_page_index - 1)
+
+func _on_page_right_button_pressed() -> void:
+	if current_page_index < (MAX_PAGES - 1):
+		show_page(current_page_index + 1)
+
+func _on_close_button_pressed() -> void:
+	pages_parent.visible = false
--- Scripts/UI scripts/grimoire_page.gd
+++ Scripts/UI scripts/grimoire_page.gd
@@ -1,24 +1,50 @@
 extends Control
-
-@onready var background: TextureRect = $PageBackground
-@onready var card_left: TextureRect = $CardLeft
-@onready var card_right: TextureRect = $CardRight
+class_name GrimoirePage
 
 @onready var tooltip_scene = preload("res://UI/Tooltip.tscn")
 var tooltip: Tooltip
 
-@export var background_texture: Texture2D
-@export var card_left_texture: Texture2D
-@export var card_right_texture: Texture2D
-
 @export var left_tooltip_text: String
 @export var right_tooltip_text: String
 
-func _ready() -> void:
-	background.texture = background_texture
-	card_left.texture = card_left_texture
-	card_right.texture = card_right_texture
+@onready var card_left: TextureRect = $CardLeft
+@onready var card_right: TextureRect = $CardRight
+
+@export var left_card_index: PuzzleManager.puzzles
+@export var right_card_index: PuzzleManager.puzzles
 
 func _process(delta: float) -> void:
 	if tooltip:
 		tooltip.global_position = get_global_mouse_position() + Vector2(0, -80)
+
+func update_cards():
+	card_left.visible = PuzzleManager.complete_puzzles[left_card_index]
+	card_right.visible = PuzzleManager.complete_puzzles[right_card_index]
+
+func _on_panel_left_mouse_entered() -> void:
+	tooltip = tooltip_scene.instantiate()
+	tooltip.set_text(left_tooltip_text)
+	tooltip.visible = false
+	add_child(tooltip)
+	await get_tree().create_timer(1).timeout
+	if tooltip:
+		tooltip.visible = true
+
+func _on_panel_left_mouse_exited() -> void:
+	if tooltip:
+		tooltip.queue_free()
+		tooltip = null
+
+func _on_panel_right_mouse_entered() -> void:
+	tooltip = tooltip_scene.instantiate()
+	tooltip.set_text(right_tooltip_text)
+	tooltip.visible = false
+	add_child(tooltip)
+	await get_tree().create_timer(1).timeout
+	if tooltip:
+		tooltip.visible = true
+
+func _on_panel_right_mouse_exited() -> void:
+	if tooltip:
+		tooltip.queue_free()
+		tooltip = null
